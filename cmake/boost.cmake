d39695ae1f76576100db42c96864a6554b117b06
Akylzhan Sauranbay <akyl.sauranbai@gmail.com>
2025-12-24 13:03:21 +0500

    boost non-static

--- cmake/boost.cmake
+++ cmake/boost.cmake
@@ -1,6 +1,6 @@
-set(Boost_USE_STATIC_LIBS ON)
-find_package(Boost 1.81.0 REQUIRED COMPONENTS thread chrono system regex system context)
+#set(Boost_USE_STATIC_LIBS ON)
+find_package(Boost REQUIRED COMPONENTS thread chrono regex context)
 
 include_directories( ${Boost_INCLUDE_DIR} )
 
-set(BOOST_LIBRARIES Boost::headers Boost::system Boost::regex Boost::thread Boost::context Boost::chrono)
+set(BOOST_LIBRARIES Boost::headers Boost::regex Boost::thread Boost::context Boost::chrono)
